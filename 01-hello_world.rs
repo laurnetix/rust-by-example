bff56583e536c916252f36170aaa9e80baf8fe59
Lauren <laurnetix@icloud.com>
2025-08-04 21:23:09 -0400

    Learn more about comments in rust and primitives

--- 01-hello_world.rs
+++ 01-hello_world.rs
@@ -1,5 +1,16 @@
 // Comments are made with double slashes
 
+/* Block comments can be made with star slash */
+
+/// Doc comments can be made with three slashes
+//! Or two slashes with an exclamation point
+
+/*
+ * Big blocks can be commented out on many lines
+ * With slashes and stars together
+ * Like this!
+ */
+
 // the main function is defined with fn main()
 fn main() {
     // functions are defined with fn
@@ -7,4 +18,7 @@ fn main() {
     // prints with println!("string")
     println!("Hello World!");
     println!("I'm a rustacean!");
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
 }
---
+++ 02-primitives.rs
@@ -0,0 +1,13 @@
+// Practice integers~!
+
+fn main() {
+
+    // x is a signed 32-bit integer with a value of 37
+    let x = 37i32;
+
+    // y is a signed 32-bit integer with a value of 52
+    let y = 52i32;
+
+    // Answer is -15
+    println!("{}", x - y);
+}
