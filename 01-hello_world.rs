76a8fd6b9ca28b1f37d035b2af96c4591c9c5eb1
Lauren <laurnetix@icloud.com>
2025-08-04 21:13:58 -0400

    Add gitignore and rust activity 1

---
+++ 01-hello_world.rs
@@ -0,0 +1,10 @@
+// Comments are made with double slashes
+
+// the main function is defined with fn main()
+fn main() {
+    // functions are defined with fn
+
+    // prints with println!("string")
+    println!("Hello World!");
+    println!("I'm a rustacean!");
+}
